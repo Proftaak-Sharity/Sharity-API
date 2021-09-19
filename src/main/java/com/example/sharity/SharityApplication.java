8b6327d22d7d8108264f1e882a2134af4c4c5527
Rob van der Horst <rplvanderhorst@gmail.com>
2021-09-19 21:00:38 +0200

    overwrote project

--- src/main/java/com/example/sharity/SharityApplication.java
+++ src/main/java/com/example/sharity/SharityApplication.java
@@ -1,5 +1,6 @@
-package nl.avans.sharity;
+package com.example.sharity;
 
+import com.example.sharity.car.Car;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class SharityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SharityApplication.class, args);
+
+        Car myCar = new Car(4);
+
+        System.out.println("My car has " + myCar.amountWheels() + " wheels!");
+
+
     }
+
 
 }
---
+++ src/main/java/com/example/sharity/car/Car.java
@@ -0,0 +1,14 @@
+package com.example.sharity.car;
+
+public class Car {
+
+    int wheels;
+
+    public Car(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public int amountWheels() {
+        return this.wheels;
+    }
+}

280cc83a006e65712629b6e82b338b7a60b5bd75
Rob van der Horst <rplvanderhorst@gmail.com>
2021-09-20 07:44:26 +0200

    Database (mySQL) instellingen toegepast.

--- src/main/java/com/example/sharity/car/Car.java
+++ src/main/java/com/example/sharity/car/Car.java
@@ -1,7 +1,26 @@
 package com.example.sharity.car;
 
+
 public class Car {
 
+    String licensePlate;
+    String make;
+    String model;
+    String bodyType;
+    String gearbox;
+    int enginePower;
+    int yearOfBuild;
+    int doors;
+    String color;
+    int seats;
+    String damage;
+    boolean available;
+    double dailyRate;
+    double pricePerKilometer;
+    double packagePrice500;
+    double getPackagePrice1000;
+
+//    test
     int wheels;
 
     public Car(int wheels) {

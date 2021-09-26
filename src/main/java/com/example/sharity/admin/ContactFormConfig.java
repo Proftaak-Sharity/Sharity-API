35870af8971ddccc2d6710fa6437593dbd31bcb1
Shabular <43166019+Shabular@users.noreply.github.com>
2021-09-26 19:46:12 +0200

    contactForm is made and put in database

--- src/main/java/com/example/sharity/admin/ContactFormConfig.java
+++ src/main/java/com/example/sharity/admin/ContactFormConfig.java
@@ -1,5 +1,6 @@
 package com.example.sharity.admin;
 
+import com.example.sharity.customer.BankAccount;
 import com.example.sharity.customer.Customer;
 import com.example.sharity.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -15,19 +16,36 @@ import java.util.Optional;
 public class ContactFormConfig {
 
     @Bean
-    CommandLineRunner commandLineRunnerContacTForm(ContactFormRepository repository) {
+    CommandLineRunner commandLineRunnerContactForm(ContactFormRepository repository) {
         return args -> {
-            CustomerRepository customerRepository = null;
-            Optional<Customer> bart = customerRepository.findCustomerByEmail("b.grootoonk@student.avans.nl");
-//            ContactForm mailError = new ContactForm(
-//                    bart,
-//                    LocalDate.of(2021, Month.SEPTEMBER, 24),
-//                    "Could not cancel my reservations\nHow can i contact the owner of the vehicle",
-//            );
-            System.out.println(bart);
-//           repository.saveAll(
-//                    List.of(mailError)
-//            );
+            Customer Henk = new Customer(
+                    "de Jong",
+                    "Henk",
+                    LocalDate.of(1983, Month.AUGUST, 29),
+                    "h.dejong@student.avans.nl",
+                    new BankAccount("NL71ABNA082343513", "H. de Jong")
+            );
+
+            ContactForm test = new ContactForm( Henk,
+                    LocalDate.of(2021, Month.SEPTEMBER, 26),
+                    "Stuff is broken joh");
+
+
+
+                    repository.save(test);
+
+//
+//            CustomerRepository customerRepository = null;
+//            Optional<Customer> bart = customerRepository.findCustomerByEmail("b.grootoonk@student.avans.nl");
+////            ContactForm mailError = new ContactForm(
+////                    bart,
+////                    LocalDate.of(2021, Month.SEPTEMBER, 24),
+////                    "Could not cancel my reservations\nHow can i contact the owner of the vehicle",
+////            );
+//            System.out.println(bart);
+////           repository.saveAll(
+////                    List.of(mailError)
+////            );
         };
     }
 }
--- src/main/java/com/example/sharity/customer/CustomerRepository.java
+++ src/main/java/com/example/sharity/customer/CustomerRepository.java
@@ -4,13 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepository
         extends JpaRepository<Customer, Long> {
 
-//    SELECT * FROM customer WHERE email = :email
+
+    //    SELECT * FROM customer WHERE email = :email
     @Query
     Optional<Customer> findCustomerByEmail(String email);
 

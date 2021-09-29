33f0b15444505862ff5e3fc866bad60aec34400a
Shabular <43166019+Shabular@users.noreply.github.com>
2021-09-29 19:30:12 +0200

    working again

--- src/main/java/com/example/sharity/admin/Servicedesk.java
+++
@@ -1,9 +0,0 @@
-package com.example.sharity.admin;
-
-public class Servicedesk {
-    // In the servicedesk we will find procedures to reply to customers
-    // Here we will put messages in the database and let the admin retrieve them
-
-
-
-}

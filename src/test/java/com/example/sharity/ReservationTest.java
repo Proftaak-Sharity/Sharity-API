66a48fc227e670f48d2597fd13f6ed38e6709c8b
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-10-12 22:13:40 +0200

    Update ReservationTest.java

--- src/test/java/com/example/sharity/ReservationTest.java
+++ src/test/java/com/example/sharity/ReservationTest.java
@@ -9,6 +9,7 @@ import com.example.sharity.repository.CarRepository;
 import com.example.sharity.service.CarService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -22,17 +23,17 @@ public class ReservationTest {
 
     private CarService sut;
 
-    @BeforeEach
-    public void beforeEach() {
-        CarRepository repository = mock(CarRepository.class);
-
-        Car car = mock(Car.class);
-        when(car.getPricePerDay()).thenReturn(150.00);
-
-        when(repository.getById(car.getLicensePlate())).thenReturn(car);
-
-        sut = new CarService(repository);
-    }
+//    @BeforeEach
+//    public void beforeEach() {
+//        CarRepository repository = mock(CarRepository.class);
+//
+//        Car car = mock(Car.class);
+//        when(Car.getPricePerDay()).thenReturn(100.00);
+//
+//        when(repository.getById(FuelCar.getLicensePlate("KNTK01"))).thenReturn(car);
+//
+//        sut = new CarService(repository);
+//    }
 
 
     @Test
@@ -43,20 +44,22 @@ public class ReservationTest {
         // Act
         int days = Period.between(reservation.getStartDate(), reservation.getEndDate()).getDays();
         // Assert
-        assertEquals(4,4);
+        assertEquals(4, days);
     }
 
-//    @Test
-//    void addReservationGetRentTest() {
-//     Car car = new FuelCar("KNTK01", Make.Volvo, "XC90", FuelType.DIESEL, 52, 12, 99.33);
-//        // Arrange
-//        Reservation reservation = new Reservation(3L,"KNTK01", 600, LocalDate.of(2021, Month.DECEMBER, 1),
-//                LocalDate.of(2021, Month.DECEMBER, 5));
-//        // Act
-//        sut.getRentFromCar("KNTK01");
-//        // Assert
-//        assertEquals(99.33,99.33);
-//    }
+    @Test
+    void addReservationGetRentTest() {
+        // Arrange
+        Car car = new FuelCar("KNTK01", Make.Volvo, "XC90", FuelType.DIESEL, 52, 12, 100.00, null);
+        Reservation reservation = new Reservation(3L,"KNTK01", 600, LocalDate.of(2021, Month.DECEMBER, 1),
+                LocalDate.of(2021, Month.DECEMBER, 5));
+        // Act
+        int days = Period.between(reservation.getStartDate(), reservation.getEndDate()).getDays();
+        double rent = car.getPricePerDay() * days;
+
+        // Assert
+        assertEquals(400.00, rent);
+    }
 
 }
 

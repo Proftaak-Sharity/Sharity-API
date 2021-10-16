4d81aaa0803ab23723bd9a6d8d7a130866341b5c
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-10-16 16:06:27 +0200

    Create ReservationRepositoryTest.java

---
+++ src/test/java/com/example/sharity/repository/ReservationRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.example.sharity.repository;
+
+import com.example.sharity.entity.reservation.Reservation;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+class ReservationRepositoryTest {
+
+    @Autowired
+    private ReservationRepository underTest;
+
+    @Test
+    void shouldFindReservationByReservationNumber() {
+        // Arrange
+        Reservation reservation = new Reservation(7L, "XX95ZR", 100, 150, 250.00, LocalDate.of(2021, Month.JANUARY, 1),
+                LocalDate.of(2021, Month.JANUARY, 5));
+        underTest.save(reservation);
+
+        // Act
+        Optional<Reservation> expected = underTest.findReservationByReservationNumber(reservation.getReservationNumber());
+        // Assert
+        assertThat(expected).isPresent();
+    }
+
+    @Test
+    void checkCarAvailability() {
+        // Arrange
+        Reservation reservation = new Reservation(3L, "KNTK01", 100, 150, 250.00, LocalDate.of(2021, Month.DECEMBER, 1),
+                LocalDate.of(2021, Month.DECEMBER, 5));
+        underTest.save(reservation);
+
+        // Act
+        Optional<Reservation> expected = underTest.checkCarAvailability("KNTK01",LocalDate.of(2021, Month.DECEMBER, 1),
+                LocalDate.of(2021, Month.DECEMBER, 5));
+        // Assert
+        assertThat(expected).isPresent();
+    }
+}

ce270a0c8438555947ac68e68424f0954c1214fb
RobvdHorst <71100038+RobvdHorst@users.noreply.github.com>
2022-01-07 21:44:37 +0100

    carImage added

---
+++ src/main/java/com/example/sharity/car/CarImage.java
@@ -0,0 +1,32 @@
+package com.example.sharity.car;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
+@Getter
+@Setter
+@ToString
+@Entity
+public class CarImage implements Serializable {
+
+    @Id
+    @Column(unique = true, length = 20, name = "license_plate")
+    private String licensePlate;
+
+    @Column(columnDefinition = "LONGBLOB")
+    private String image;
+
+    public CarImage(String licensePlate, String image) {
+        this.licensePlate = licensePlate;
+        this.image = image;
+    }
+
+    public CarImage() {
+    }
+}
